695545a9372b2247a2bd82196f283d7b6d2bdb41
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-09-01 21:12:12 +0530

    Implimented API gateway

---
+++ System_Design/phase2/backend/gateway/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import proxy from "express-http-proxy"
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: `hello from Backend{Gateway}`,
+  });
+});
+
+app.use("/auth", proxy("http://localhost:8001"));
+app.use("/order", proxy("http://localhost:8002"));
+app.use("/product", proxy("http://localhost:8003"));
+
+
+app.listen(port, () => {
+  console.log(`server started ${port}`);
+});
---
+++ System_Design/phase2/backend/services/auth/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: `hello from auth server`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server started ${port}`);
+});
