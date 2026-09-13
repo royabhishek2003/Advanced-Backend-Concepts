113f96e27ec2dc830ab2272a834c68c5c9eb0eb0
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-09-14 00:37:44 +0530

    Langgraph Ai workFlow defines

---
+++ AI/index.js
@@ -0,0 +1,143 @@
+import express from "express"
+import dotenv from "dotenv"
+import {GoogleGenAI} from "@google/genai"
+import { ChatGroq } from "@langchain/groq"
+
+import {StateGraph, Annotation} from "@langchain/langgraph"
+const app = express();
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+
+dotenv.config();
+app.use(express.json()); 
+
+// without langchain
+
+// making a LLM call using SDK's
+
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY
+// });
+// // no real time data and no meory just stateless 
+// const main = async(input) =>{
+//      try{
+//         const response = await ai.models.generateContent({ 
+//         model:"gemini-3.5-flash",
+//         contents:[
+//             {role:"system",
+//                 parts:[{text:"you are an assistant and your name is jarvis. If you don't kow the answer don't give wrong answer"}]
+//             },  
+//             {role:"user",
+//                 parts:[{text:input}]
+//             }
+//         ]
+//      })
+//         return response;
+//      }
+//      catch(error){
+//         console.log(`Error occured while calling geminiai ${error}`);
+//      }
+// }
+
+
+
+
+// without langchain 
+
+// app.post("/ai", async(req, res)=>{
+//     try{
+//         const {input}= req.body;
+//         const response= await main(input);
+//         if(response.text){
+//             return res.status(200).json(response.text);
+//         }
+//         return res.status(500).json({
+//             "Message":"Sorry i can not help in this matter"
+//         })
+//     }catch(error){
+//         return res.status(500).json({"message":"Internal Server Error"});
+//     }
+    
+// })
+
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        "message":"Hello from level1"
+    })
+})
+
+
+// with langchain 
+
+
+const llm = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temrature:0,  // jitna kam temprature rakhenge llm utna serius rhega aur temrature jyada rakhne pr model creative type answer dega 
+    maxRetries: 2,  // 
+    maxTokens:100
+})
+
+
+const aksai = async(input) => {
+    const aiMsg = await llm.invoke([
+    [
+        "system",
+        "You are a Assistant your name is Jarvis",
+    ],
+    ["human", input],
+])
+    return aiMsg;
+}
+
+
+
+// make a custom state for langgraph 
+
+const State = Annotation.Root({
+    prompt:Annotation, // annotation -> stores the value return by the most recent node 
+    aiMsg:Annotation
+})
+
+
+// now we need to create a tool node so that ai can use that tool 
+
+
+const callLLM= async(state)=>{
+    try{
+        console.log("state: ",state);
+        const input = state.prompt;
+        const response = await aksai(input)
+
+        return {
+            aiMsg:response.content
+        }
+    }catch(error){
+        throw new Error(`LLM call failed: ${error.message}`);
+}}
+
+const graph = new StateGraph(State)
+                .addNode("agent",callLLM)
+                .addEdge("__start__","agent")
+                .addEdge("agent","__end__")
+                .compile()
+
+
+
+app.post("/ai", async(req, res)=>{
+    try{
+        const {input}= req.body;
+        const response = await graph.invoke({prompt:input})
+        console.log(response.aiMsg);
+        return res.status(200).json({
+            "reply":response.aiMsg
+        })
+    }catch(error){
+        return res.status(500).json({"message":`Internal Server Error ${error}`});
+    }
+    
+})               
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is started");
+})
